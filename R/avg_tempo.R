ecae9faba30d6387865d48f02600eaf8e6c42316
Daniel Shanahan <danielshanahan@Daniels-MBP.lan>
2020-05-22 13:03:12 -0400

    more test function

---
+++ R/avg_tempo.R
@@ -0,0 +1,15 @@
+#' @name avg_tempo
+#' @title Looking at average tempi by a number of selections and groups.
+#' @description
+#' This function provides an easy eay to get the average tempo by a number of groups.
+#' You can look at tempo by measure, beat, composer, or piece.
+#'
+#' @param data, The dataframe
+#' @param by, grouping operator
+#' @param selection Filtered selection.
+#' @export
+
+avg_tempo <- function(data, by=measure.beat, selection=""){
+
+
+}
---
+++ todo.md
@@ -0,0 +1,9 @@
+1. Average tempo by artist (global) avg_tempo(data, by=beat, artist, measure, piece, etc, selection="1.1-1.4")
+2. Average flexibility by artist (global) avg_flex(data, by=beat, artist, measure, piece, etc)
+3. onset to bpm 
+4. easier way to filter data by measure and beat.
+5. density plot density_plot(data, by=beat, artist, measure, piece)
+
+
+
+avg_tempo(mahler1, by=conductor, selection="51.1-51.4")
